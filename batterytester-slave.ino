ad882007cb57e134993a7719f263c52517af9d1e
Daniel Römer <daromer@gmail.com>
2018-10-27 14:51:06 +0200

    Added some variables

--- batterytester-slave.ino
+++ batterytester-slave.ino
@@ -16,7 +16,13 @@
 
 #include "Timer.h"
 
+
+// Constant Variables
+const byte cells = 1; // Number of cells to monitor
+
 bool runningTest = false; //If we have anything running and being tested
+
+int unitID = 0;
 
 int  serIn;             // var that will hold the bytes-in read from the serialBuffer
 char serInString[100];  // array that will hold the different bytes  100=100characters;
@@ -24,6 +30,18 @@ char serInString[100];  // array that will hold the different bytes  100=100char
 int  serInIndx  = 0;    // index of serInString[] in which to insert the next incoming byte
 int  serOutIndx = 0;    // index of the outgoing serInString[] array;
 
+byte mosfetSwitchState[cells]; // Is it idle, discharging or charging?
+float batteryVoltage[cells];
+float batteryLastVoltage[cells];
+byte batteryFaultCode[cells];
+byte batteryInitialTemp[cells];
+byte batteryHighestTemp[cells];
+byte batteryCurrentTemp[cells];
+int batteryCapacity[cells];
+int batteryTimeCharge[cells];
+int batteryTimeDisCharge[cells];
+int batteryIr[cells];
+byte tempCount[cells];
 
 void setup() {
 // inite serial to handle the daisy chain
@@ -36,6 +54,7 @@ Serial.begin(9600);
 timerObject.every(2, cycleSerial);  // Read data from serial to buffer
 timerObject.every(2, cycleSerialBuffer);  //Process data from buffer
 timerObject.every(2000, cycleValues);  // Process battery data
+timerObject.every(4000, cycleValuesOut);  // Process battery data
 
 
 timerObject.every(10000, cycleAlive);
@@ -53,10 +72,22 @@ void cycleAlive() {
     Serial.println("Im alive. Dont kill me");
   }
 }
+
 // This cycles through all values to read from the battery
 void cycleValues() {
   
 }
+
+// This cycles through all values to read from the battery
+void cycleValuesOut() {
+
+  // Lets send out the values on the grid
+
+
+  Serial.print(unitID);
+  Serial.println(" Data........");
+}
+
 //Reads all data from the serial port into buffer
 void cycleSerial() {
       char sb;   
